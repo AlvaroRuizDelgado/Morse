fc0ca957d8607e3f629214cff1f28e5c2ab6d67e
AlvaroRuizDelgado <alvaro.ruiz.delgado@gmail.com>
2017-03-01 17:38:18 +0900

    Updated procedures

--- install.sh
+++ install.sh
@@ -5,22 +5,6 @@ sudo apt-get update
 sudo apt-get -y install python-virtualenv
 
 # Create folder and 
-mkdir morse
-cd morse
 virtualenv venv
 . venv/bin/activate
 pip install Flask
-
-# Create the hello world script.
-cat > hello_world.py << EOF
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return '\nHello, World!\n\n'
-EOF
-
-# Run it
-export FLASK_APP=hello_world.py
-flask run --host=0.0.0.0
