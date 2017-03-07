b4769bcc54e4ad255ae1d569f33f5ae7aafa6872
AlvaroRuizDelgado <alvaro.ruiz.delgado@gmail.com>
2017-03-07 10:48:33 +0900

    Sudo wipes the env variables, fixed run_flask

--- Heat/initial_config.sh
+++ Heat/initial_config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo "script initiated"
-touch started
+echo "user_data initiated"
 sudo apt-get update
 # cd /home/ubuntu/
 # echo "export PS1=\"\t \[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\\$ \"" >> .bash_profile
@@ -10,3 +9,4 @@ git clone https://github.com/AlvaroRuizDelgado/Morse.git
 cd Morse
 . install.sh
 . run_flask
+echo "user_data finished"
