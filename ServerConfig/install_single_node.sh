6c652f747d9de7d9033762dc03ba7a9638e4d301
AlvaroRuizDelgado <alvaro.ruiz.delgado@gmail.com>
2017-03-12 18:35:05 +0900

    Database for single node

---
+++ ServerConfig/install_single_node.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Update and install
+sudo apt update
+# sudo apt-get dist-upgrade
+sudo apt -y install python-virtualenv
+
+# Create virtual environment and install Flask in it
+virtualenv venv --system-site-package
+. venv/bin/activate
+pip install Flask
+. run_flask
