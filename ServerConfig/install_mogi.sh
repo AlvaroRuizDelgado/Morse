ea14b0e8f5a3447b31dea796d6f3e9d70fd8112b
AlvaroRuizDelgado <alvaro.ruiz.delgado@gmail.com>
2017-03-13 11:28:16 +0900

    Adequations after merge.

---
+++ ServerConfig/install_mogi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Update and install
+sudo apt update
+# sudo apt-get dist-upgrade
+sudo apt -y install python-virtualenv
+
+# Create virtual environment and install Flask in it
+virtualenv venv --system-site-package
+. venv/bin/activate
+pip install Flask
+pip install flask-mysql
+. run_flask
